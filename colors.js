0968dbbfb779a2765c98236910ca30e47f2562c9
egg93 <55375216+egg93@users.noreply.github.com>
2021-10-21 17:35:01 +0900

    로드맵 3 javascript 정리 및 실습
    마무리 파트 부분 업로드

---
+++ colors.js
@@ -0,0 +1,34 @@
+    var Links = {
+      setColor: function(color){
+       //var aList = document.querySelectorAll('a');
+       //var i = 0;
+       //while (i<aList.length) {
+        //aList[i].style.color=color;
+        //i = i+1;
+      //}
+      $('a').css('color', color);
+    }
+  }
+  var Body = {
+    setColor: function(color){
+      //document.querySelector('body').style.color=color;
+      $('body').css('color', color);
+    },
+    setBackgroundColor: function(color){
+     //document.querySelector('body').style.backgroundColor=color;
+     $('body').css('backgroundColor', color);
+    }
+  }
+  function nightDayHandler(self){
+    if (self.value === 'this.night'){
+      Body.setBackgroundColor('black');
+      Body.setColor('white');
+      self.value='this.day';
+      Links.setColor('powderBlue');
+    } else {
+      Body.setBackgroundColor('white');
+      Body.setColor('black');
+      self.value='this.night';
+      Links.setColor('blue');
+    }
+  }
